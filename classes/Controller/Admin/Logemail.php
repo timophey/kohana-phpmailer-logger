4a98cbe7bf9de966f21ddccd541a403c12c4d086
timophey <timophey_@mail.ru>
2015-12-08 19:04:50 +0500

    Поправил ведущий ноль в 04.log, спрятал дебаг

--- classes/Controller/Admin/Logemail.php
+++ classes/Controller/Admin/Logemail.php
@@ -7,11 +7,7 @@ class Controller_Admin_Logemail extends Controller_Admin_Common {
 		$today = [date('Y'),date('m'),date('d')];
 		list($y,$m,$d) = $today;
 		if($s=Arr::get($_GET,'date')) if(preg_match("/^(\\d{4})(\\d{2})(\\d{2})$/",$s,$p)) list($y,$m,$d) = array($p[1],$p[2],$p[3]);
-		//profilertoolbar::adddata([$today,$y,$m,$d]);
-		
-		//$dir = APPPATH.'logs/email/'.date('Y_m');
-		//$file = date('d').'.log';
-		$path = sprintf("%slogs/email/%d_%d/%d.log",APPPATH,$y,$m,$d);
+		$path = sprintf("%slogs/email/%d_%s/%s.log",APPPATH,$y,$m,$d);
 		if(file_exists($path)){
 			$file_contents = file_get_contents($path);
 			$strings = explode("\r\n",$file_contents);
@@ -19,7 +15,6 @@ class Controller_Admin_Logemail extends Controller_Admin_Common {
 				$this->view->items = array_map(function($str){
 					$assoc = json_decode($str,true);
 					$assoc['datetime']=date('Y-m-d H:i:s',$assoc['timestamp']);
-					//profilertoolbar::adddata($assoc);
 					return $assoc;
 					},$strings);
 				}
@@ -29,14 +24,9 @@ class Controller_Admin_Logemail extends Controller_Admin_Common {
 		if($filelist) foreach($filelist as $path){
 			preg_match('/^(\/.*)*\/(\\d{4})_(\\d{2})\/(\\d{2})\.log$/',$path,$p);
 			$p[5] = ([$y,$m,$d] == [$p[2],$p[3],$p[4]]); // active flag
-			profilertoolbar::adddata([[$y,$m,$d],[$p[2],$p[3],$p[4]]]);
 			$daylist[]= $p;
 			} else $daylist=null;
 		$this->view->daylist = $daylist;
-//		profilertoolbar::adddata($this->view);
-//		profilertoolbar::adddata($filelist);
-		profilertoolbar::adddata($daylist);
-
 		}
 	/*public function action*/
 	}
